c0b2238e01c944d834f364baf14802aba1c7c749
hw090729160 <hw090729160@users.noreply.github.com>
2026-08-03 04:53:31 +0800

    修复构建: 使用Retrofit官方kotlinx-serialization转换器
    - 替换不存在的com.jakewharton:retrofit2-kotlinx-serialization-converter:1.0.1
    - 使用官方com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0
    - 更新NetworkModule中的import路径

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.1")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
--- android/app/src/main/java/com/swimanalysis/app/di/NetworkModule.kt
+++ android/app/src/main/java/com/swimanalysis/app/di/NetworkModule.kt
@@ -12,7 +12,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
